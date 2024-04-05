3a27cd175b85d717359710b8d10df3eee6f5737b
MaxiMenfi45 <maximenfi@outlook.es>
2024-04-05 19:10:21 -0300

    Proyecto terminado y funcional, agregue la logica del conversor

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const number = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
-const pOutput = document.getElementById("p-output");
 const answer  ={
     answer1: "Please enter a valid number",
     answer2: "Please enter a number greater than or equal to 1",
     answer3: "Please enter a number less than or equal to 3999"
 }
+const numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+const letters = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+let result = '';
 
 
 const checker = (n) =>{
     if(n === ""){
-        removeHidden();
-        pOutput.innerHTML = answer.answer1;
+        
+        output.innerHTML = answer.answer1;
     }
     else if(n < 0 ){
-        removeHidden();
-        pOutput.innerHTML = answer.answer2;
+        
+        output.innerHTML = answer.answer2;
+        output.style.height="120px";
     }
     else if(n > 3999){
-        removeHidden();
-        pOutput.innerHTML=answer.answer3;
+        
+        output.innerHTML=answer.answer3;
+        output.style.height="120px";
     }
-    else{
-        converter();
+    else {
+        converter(n);
     }
+    removeHidden();
 }
 
 convertBtn.addEventListener("click",()=>{
@@ -33,6 +38,27 @@ convertBtn.addEventListener("click",()=>{
 });
 const removeHidden = ()=>{
     output.classList.remove("hidden");
+}
+
+const converter = (n) =>{
+
+        result='';
+        let index = 0;
+
+        while(n > 0){
+            console.log("hola");
+            if(n>=numbers[index]){
+                result += letters[index];
+                n -= numbers[index];
+            }
+            else{
+                index++;
+            }
+        }
+        output.innerHTML = result; 
+        removeHidden();
+
+    
 }
 
 
